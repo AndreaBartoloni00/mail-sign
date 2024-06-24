73328304ca90d18e20be638d7b7377b10ae9deb3
AndreaBartoloni <andrea.bartoloni@degg.it>
2024-06-24 15:42:44 +0200

    install vite and chadcn

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,39 @@
-<script setup>
-import { ref } from 'vue'
+<script setup lang="">
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+import { ref } from 'vue';
+    import { h } from 'vue'
+    import { useForm } from 'vee-validate'
+    import { toTypedSchema } from '@vee-validate/zod'
+    import * as z from 'zod'
+import { Button } from '@/components/button'
+    import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    } from '@/components/form'
+    import { Input } from '@/components/input';
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <form @submit="onSubmit">
+    <FormField v-slot="{ componentField }" name="username">
+      <FormItem>
+        <FormLabel>Username</FormLabel>
+        <FormControl>
+          <Input type="text" placeholder="shadcn" v-bind="componentField" />
+        </FormControl>
+        <FormDescription>
+          This is your public display name.
+        </FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+    <Button type="submit">
+      Submit
+    </Button>
+  </form>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
